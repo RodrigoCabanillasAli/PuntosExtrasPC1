f0f1a3676cacdc1bb8dd69f264502e34ff256cc5
RodrigoCabanillasAli <rodrigocabanillasv@gmail.com>
2026-04-25 21:09:20 -0500

    Se hizo una prueba para comprobar mi github

---
+++ src/puntosextras/prueba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package puntosextras;
+
+/**
+ *
+ * @author METRO COMAS
+ */
+public class prueba {
+    public static void main(String[] args) {
+        System.out.println("Prueba para github");
+    }
+            
+}
